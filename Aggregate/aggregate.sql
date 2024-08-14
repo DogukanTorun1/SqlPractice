d4500bbdcf98b878dddc8560154e9bc63cd99153
Dogukan <62773717+DogukanTorun1@users.noreply.github.com>
2024-08-14 23:46:36 +0300

    Update aggregate.sql

--- Aggregate/aggregate.sql
+++ Aggregate/aggregate.sql
@@ -1,1 +1,39 @@
+-- Query the sum of film durations
+SELECT SUM(duration) as total_duration
+FROM films;
 
+-- Calculate the average duration of all films
+SELECT AVG(duration) as average_duration
+FROM films;
+
+-- Find the latest release_year
+SELECT MAX(release_year) as latest_year
+FROM films;
+
+-- Calculate the sum of gross from the year 2000 or later
+SELECT SUM(gross) as total_gross
+FROM films
+WHERE release_year >= 2000;
+
+-- Calculate the average gross of films that start with A
+SELECT AVG(gross) as avg_gross_A
+FROM films
+WHERE title LIKE ('A%');
+
+-- Calculate the lowest gross film in 1994
+SELECT MIN(gross) as lowest_gross
+FROM films
+WHERE release_year = 1994;
+
+-- Calculate the highest gross film released between 2000-2012
+SELECT MAX(gross) as highest_gross
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
+
+-- Round the average number of facebook_likes to one decimal place
+SELECT ROUND(AVG(facebook_likes), 1) as avg_facebook_likes
+FROM reviews
+
+-- Calculate the average budget rounded to the thousands
+SELECT ROUND((AVG(budget)) , -3) as avg_budget_thousands
+FROM films
