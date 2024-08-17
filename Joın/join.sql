39206e362727be7880bbdbb0995607b42620fb93
Dogukan <62773717+DogukanTorun1@users.noreply.github.com>
2024-08-17 03:39:58 +0300

    Update join.sql

--- Joın/join.sql
+++ Joın/join.sql
@@ -1,1 +1,12 @@
+-- Inner join to countries, Match on country codes
+SELECT * 
+FROM cities
+INNER JOIN countries
+ON cities.country_code = countries.code;
+
+-- Select fields with aliases, Join to economies (alias e), Match on code field using table aliases
+SELECT c.code as country_code, c.name, e.year, e.inflation_rate
+FROM countries AS c
+INNER JOIN economies as e
+ON c.code = e.code
 
